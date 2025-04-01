51babc7781bf3c980db8560221c86f878de73ffb
iaroslav_shavrov <iaroslav_shavrov@practice.relex.ru>
2025-04-01 22:46:07 +0300

    Added first tests

---
+++ src/components/Button.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Button from './Button';
+import { vi } from 'vitest';
+
+describe('Button Component', () => {
+    it('renders кнопку с правильным текстом', () => {
+        render(<Button label="Нажми меня" onClick={() => {}} />);
+        expect(screen.getByText('Нажми меня')).toBeInTheDocument();
+    });
+
+    it('вызывает onClick при клике', async () => {
+        const handleClick = vi.fn();
+        render(<Button label="Нажми меня" onClick={handleClick} />);
+        await userEvent.click(screen.getByText('Нажми меня'));
+        expect(handleClick).toHaveBeenCalledTimes(1);
+    });
+});
