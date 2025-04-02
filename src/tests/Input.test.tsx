a5a8eaa65980500322483fa3cba0ee9b3d82c6ee
iaroslav_shavrov <iaroslav_shavrov@practice.relex.ru>
2025-04-02 10:42:10 +0300

    Add beforeEach test

--- src/tests/Input.test.tsx
+++ src/tests/Input.test.tsx
@@ -5,43 +5,46 @@ import FormItem from "../components/FormItem";
 import Input from "../components/Input";
 
 describe("Default Input component", () => {
+    let input: HTMLElement;
+
+    beforeEach(() => {
+        render(<Input />);
+        render(<FormItem />);
+        input = screen.getByLabelText(/text/i)
+    })
     // Рендерится ли компонент input
     it("Render", () => {
-        render(<FormItem />);
-        const input = screen.getByLabelText(/text/i);
         expect(input).toBeInTheDocument();
     })
     // Имеет ли правильный тип
     it('Attribute type=text', () => {
-        render(<FormItem />)
-        const input = screen.getByLabelText(/text/i);
         expect(input).toHaveAttribute('type', 'text');
     })
     // Проверка на пустоту
     it('Check empty', () => {
-        render(<Input />);
         expect(screen.getByRole('textbox')).toHaveValue('');
     });
     // проверка аттрибута placeholder
     it('should have empty placeholder by default', () => {
-        render(<Input />);
         expect(screen.getByRole('textbox')).toHaveAttribute('placeholder');
     });
 })
 
 describe('User interactions with Input', () => {
+    let input: HTMLElement;
+
+    beforeEach(() => {
+        render(<FormItem />);
+        input = screen.getByLabelText(/text/i)
+    })
     // Посимвольный пользовательский ввод
     it('Input text', async () => {
-        render(<FormItem />);
-        const input = screen.getByLabelText(/text/i);
         const message = 'Hello';
         await userEvent.type(input, message); // имитация посимвольного ввода
         expect(input).toHaveValue(message);
     })
     // Проверка на ивент focus
     it('Focus', async () => {
-        render(<FormItem />);
-        const input = screen.getByLabelText(/text/i);
         await userEvent.click(input);
         expect(input).toHaveFocus();
     })
